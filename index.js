1c9b283ae638cacf656e68cbb80d026114eb2ba6
Vlad Nicula <vladnicula@gmail.com>
2017-01-28 12:27:55 +0200

    Nodejs express server setup

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
